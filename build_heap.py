3fc4349d00e2df77b12141b5136db9360c34eb4b
EdgarsBleiders <125837153+EdgarsBleiders@users.noreply.github.com>
2023-03-19 23:19:01 +0000

    Edgars Bleiders

--- build_heap.py
+++ build_heap.py
@@ -1,13 +1,28 @@
 # python3
-
+def sift_down(data, i, swaps):
+    min = i
+    left = 2*i+1
+    if left < len(data) and data[left] < data[min]:
+        min = left
+    
+    right = 2*i+2
+    if right < len(data) and data[right] < data[min]:
+        min = right
+    
+    if i!=min:
+        swaps.append((i,min))
+        data[i], data[min] = data[min], data[i]
+        sift_down(data, min, swaps)
 
 def build_heap(data):
     swaps = []
     # TODO: Creat heap and heap sort
     # try to achieve  O(n) and not O(n2)
-
-
+    for i in range(len(data) // 2, -1, -1):
+        sift_down(data, i, swaps)
     return swaps
+
+
 
 
 def main():
@@ -30,10 +45,10 @@ def main():
 
     # TODO: output how many swaps were made, 
     # this number should be less than 4n (less than 4*len(data))
-
+    assert len(swaps) <=4*n
+    print(len(swaps))
 
     # output all swaps
-    print(len(swaps))
     for i, j in swaps:
         print(i, j)
 
